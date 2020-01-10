0a08409a795d77b690f371734e3e25adf0fa3fb2
Kevin Goslar <kevin.goslar@gmail.com>
2020-01-10 05:00:24 -0600

    Make tests consistent

--- test/prettify-test.ts
+++ test/prettify-test.ts
@@ -1,6 +1,6 @@
 import { assert } from "chai"
 import { prettify } from "../src/prettify"
 
-test("prettify", () => {
+test("prettify", function() {
   assert.equal(prettify("a  =1", "filename.ts", {}), "a = 1;\n")
 })
