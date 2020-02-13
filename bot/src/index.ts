3246f610977eed439baf15f898ddbd2d28566d38
Kevin Goslar <kevin.goslar@gmail.com>
2020-02-13 17:48:48 -0500

    Simplify bot startup

--- bot/src/index.ts
+++ bot/src/index.ts
@@ -2,10 +2,8 @@ import * as probot from "probot"
 import { onPullRequest } from "./on-pull-request"
 import { onPush } from "./on-push"
 
-function startBot(app: probot.Application) {
+export = function startBot(app: probot.Application) {
   app.on("push", onPush)
   app.on("pull_request.opened", onPullRequest)
   console.log("PRETTIFIER STARTED")
 }
-
-export = startBot
