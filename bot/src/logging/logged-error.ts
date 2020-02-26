bca5bef37df88c86d47da087d927ce628a64756b
Kevin Goslar <kevin.goslar@gmail.com>
2020-02-26 07:18:20 -0500

    LoggedError needs no constructor arguments

--- bot/src/logging/logged-error.ts
+++ bot/src/logging/logged-error.ts
@@ -2,4 +2,8 @@
  * LoggedError is an error that was already logged.
  * It interrupts control flow but doesn't need further logging when caught.
  */
-export class LoggedError extends Error {}
+export class LoggedError extends Error {
+  constructor() {
+    super()
+  }
+}
